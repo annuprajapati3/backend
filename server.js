d1b284d84383d5c06347c852af5ef5597551612b
annuprajapati3 <annuprajapati0004@gmail.com>
2026-03-09 23:09:34 +0530

    updated server.js

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ mongoose.connect(process.env.MONGO_URI)
 .catch(err => console.log(err));
 
 app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/auth", require("./routes/authDoctorProfile"));
+app.use("/api", require("./routes/authDoctorProfile"));
 app.listen(PORT, () => console.log("Server running on port 5000"));
